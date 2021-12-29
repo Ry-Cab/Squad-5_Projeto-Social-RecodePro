12720a7a248df0cfdf9fd1982848929788aa88d8
Ryan Cabral <cabral.reis23@gmail.com>
2021-12-29 11:54:43 -0300

    teste

---
+++ Recode-Pro-Squad-5-ProjetoSocial/site-projetosocial-squad5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace site_projetosocial_squad5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
